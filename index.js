2203fc04a148a1db6f5a5ce58c6196956c611709
iamharry <harender_24@hotmail.com>
2020-02-04 06:29:48 +0530

    BoilerPlate

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+  const response = await axios.get("http://omdbapi.com/", {
+    params: {
+      apikey: "b6c8b899",
+      s: searchTerm
+    }
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+let timeoutId;
+const onInput = event => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+input.addEventListener("input", onInput);
